2df3f1623403995aa9e312b0a3c14c80605915af
YerToffeeRoll <sam_killen_@hotmail.com>
2020-04-15 13:36:21 +0100

    Whoopsy - forgot to commit the tests with the controller

--- tests/Feature/Auth/RegisterControllerTest.php
+++ tests/Feature/Auth/RegisterControllerTest.php
@@ -1,22 +1,68 @@
 <?php
-
 namespace Tests\Feature\Auth;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    public function testRegisterSuccessfully()
     {
-        $response = $this->get('/');
+        $register = [
+            'name' => 'UserTest',
+            'email' => 'user@test.com',
+            'password' => 'testpass',
+            'c_password' => 'testpass',
+        ];
 
-        $response->assertStatus(200);
+        $this->json('POST', 'api/register', $register)
+        ->assertStatus(200)
+        ->assertJsonStructure([
+                  'success'=>[
+                    'token',
+                    'user' => [
+                        'id',
+                        'name',
+                        'email',
+                        'created_at',
+                        'updated_at'
+                    ]
+              ],
+            ]);
+    }
+
+    public function testRequireNameEmailAndPassword()
+    {
+        $this->json('POST', 'api/register')
+            ->assertStatus(422)
+            ->assertJson([
+                'error' => [
+                    'name' => ['The name field is required.'],
+                    'email' => ['The email field is required.'],
+                    'password' => ['The password field is required.'],
+                ]
+            ]);
+    }
+
+    public function testRequirePasswordConfirmation()
+    {
+        $register = [
+            'name' => 'User',
+            'email' => 'user@test.com',
+            'password' => 'userpass',
+            'c_password' => 'userpass_123'
+        ];
+
+        $this->json('POST', 'api/register', $register)
+            ->assertStatus(422)
+            ->assertJson([
+                "error"=> [
+                    "c_password" => [
+                        "The c password and password must match."
+                    ]
+                ]
+            ]);
     }
 }
