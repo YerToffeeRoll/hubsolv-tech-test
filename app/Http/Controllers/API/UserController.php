01a994f2f76dcc4700923ba8d98cb96920ea4890
YerToffeeRoll <sam_killen_@hotmail.com>
2020-04-15 13:03:07 +0100

    commeted up user login code

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,16 +10,23 @@ class UserController extends Controller
 public $successStatus = 200;
 /**
      * login api
-     * 
+     *
      * @return \Illuminate\Http\Response
      */
     public function login(){
+
+        //if email and password are authenticated
         if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+            //ger user object
             $user = Auth::user();
+            //create a success payload and create a valid api token
             $success['token'] =  $user->createToken('MyApp')-> accessToken;
+
+            //return a successful json response
             return response()->json(['success' => $success], $this-> successStatus);
         }
         else{
+          //else return an error unauthorized
             return response()->json(['error'=>'Unauthorised'], 401);
         }
     }
