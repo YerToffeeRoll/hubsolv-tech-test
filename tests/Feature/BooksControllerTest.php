647960dd4dfba2b5267f5aa86d59b33790a45d9c
YerToffeeRoll <sam_killen_@hotmail.com>
2020-04-15 16:51:08 +0100

    Testing for the filter function.

--- tests/Feature/BooksControllerTest.php
+++ tests/Feature/BooksControllerTest.php
@@ -26,4 +26,60 @@ class BooksControllerTest extends TestCase
                'success' => ['books'],
        ]);
      }
+
+
+     /**
+      * Test filtering of the API by author .
+      *
+      * @return void
+      */
+      public function testLoginAndFilterBooksByAuthor()
+      {
+        $user = ['email' => 'user@email.com','password' => 'userpass'];
+             Auth::attempt($user);
+             $token = Auth::user()->createToken('nfce_client')->accessToken;
+             $headers = ['Authorization' => "Bearer $token"];
+             $this->json('GET', 'api/book/filter?', ['author'=>'Robin Nixon'], $headers)
+                 ->assertStatus(200)
+                 ->assertJsonStructure([
+                'success' => ['books'],
+        ]);
+      }
+
+      /**
+       * Test filtering of the api by categry .
+       *
+       * @return void
+       */
+       public function testLoginAndFilterBooksByCategory()
+       {
+         $user = ['email' => 'user@email.com','password' => 'userpass'];
+              Auth::attempt($user);
+              $token = Auth::user()->createToken('nfce_client')->accessToken;
+              $headers = ['Authorization' => "Bearer $token"];
+              $this->json('GET', 'api/book/filter?', ['category'=>'Linux'], $headers)
+                  ->assertStatus(200)
+                  ->assertJsonStructure([
+                 'success' => ['books'],
+         ]);
+       }
+
+
+       /**
+        * Test filtering of the api by categry .
+        *
+        * @return void
+        */
+        public function testLoginAndFilterBooksByAuthorAndCategory()
+        {
+          $user = ['email' => 'user@email.com','password' => 'userpass'];
+               Auth::attempt($user);
+               $token = Auth::user()->createToken('nfce_client')->accessToken;
+               $headers = ['Authorization' => "Bearer $token"];
+               $this->json('GET', 'api/book/filter?', ['author'=>'Robin Nixon','category'=>'PHP'], $headers)
+                   ->assertStatus(200)
+                   ->assertJsonStructure([
+                  'success' => ['books'],
+          ]);
+        }
 }
