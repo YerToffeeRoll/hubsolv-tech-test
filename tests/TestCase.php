10226f49fefbe4544f02d3d85517ae0d37073bbb
YerToffeeRoll <sam_killen_@hotmail.com>
2020-04-15 12:55:21 +0100

    Alter the testcase to prepare the project for testing.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,20 @@
 <?php
 
+
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+  use CreatesApplication, DatabaseMigrations;
+
+    protected function setUp(): void
+    {
+       parent::setUp();
+       Artisan::call('db:seed');
+       Artisan::call('passport:install');
+    }
 }
